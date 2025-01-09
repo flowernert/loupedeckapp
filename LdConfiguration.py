c8644721dd53ec4d5c4e55678f1a0809292021a7
Florian <flo.wernert@gmail.com>
2025-01-09 12:36:51 +0100

    add rotate actions for encoder, ccw = counterclockwise, cw = clockwise

--- LdConfiguration.py
+++ LdConfiguration.py
@@ -7,8 +7,12 @@ class LdConfiguration(QObject):
   def __init__(self):
     super().__init__()
     self.profile = "default"
-    self.actions = {"enc1L": "", "enc2L": "", "enc3L": "",
-                    "enc1R": "", "enc2R": "", "enc3R": "",
+    self.actions = {"enc1L-p": "", "enc1L-ccw": "", "enc1L-cw": "",
+                    "enc2L-p": "", "enc2L-ccw": "", "enc2L-cw": "",
+                    "enc3L-p": "", "enc3L-ccw": "", "enc3L-cw": "",
+                    "enc1R-p": "", "enc1R-ccw": "", "enc1R-cw": "",
+                    "enc2R-p": "", "enc2R-ccw": "", "enc2R-cw": "",
+                    "enc3R-p": "", "enc3R-ccw": "", "enc3R-cw": "",
                     "dis1L": "", "dis2L": "", "dis3L": "",
                     "dis1R": "", "dis2R": "", "dis3R": "",
                     "tb11": "", "tb12": "", "tb13": "", "tb14": "",
